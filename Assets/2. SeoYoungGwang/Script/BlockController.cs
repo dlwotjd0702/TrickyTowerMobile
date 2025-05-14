b846e9bcd15b1ab0a3d8c1eae7d81dd6320f7e7b
ykseo88 <ykseo88@gmail.com>
2025-05-14 18:04:01 +0900

    스폰, 조작 기능 추가, 분류
    SpawnManager, InputManager 추가
    블록 조작 기능 Block에서 InputManager로 이전
    스폰 기능 추가

--- Assets/2. SeoYoungGwang/Script/BlockController.cs
+++ Assets/2. SeoYoungGwang/Script/BlockController.cs
@@ -5,67 +5,28 @@ using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
+    private InputManager inputManager;
+    private SpawnManager spawnManager;
     
-    [SerializeField] private float moveDistance;
-    [SerializeField] private float downSpeed;
-    [SerializeField] private float downSpeedMutiple;
     private Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
     {
         transform.TryGetComponent(out rigidBody);
+        inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
+        spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
         rigidBody.isKinematic = true;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        MoveInput();
-        SpinInput();
-        DownMove();
-    }
-
-    private void DownMove()
-    {
-        transform.position -= new Vector3(0, downSpeed * Time.deltaTime, 0);
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         rigidBody.isKinematic = false;
         gameObject.transform.TryGetComponent(out BlockController controller);
         gameObject.transform.TryGetComponent(out BoxCollider2D collider);
+        spawnManager.OnSpawn = true;
         collider.isTrigger = false;
         controller.enabled = false;
-    }
-
-    private void MoveInput()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            gameObject.transform.position += Vector3.left * moveDistance;
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            downSpeed *= downSpeedMutiple;
-        }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            downSpeed /= downSpeedMutiple;
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            gameObject.transform.position += Vector3.right * moveDistance;
-        }
-    }
-
-    private void SpinInput()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
-        {
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, gameObject.transform.rotation.eulerAngles.z + 90);
-        }
     }
 }
---
+++ Assets/2. SeoYoungGwang/Script/InputManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject currentBlock;
+    private SpawnManager spawnManager;
+    
+    [SerializeField] private float moveDistance;
+    [SerializeField] private float downSpeed;
+    [SerializeField] private float downSpeedMutiple;
+    
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        if (currentBlock == null)
+        {
+            currentBlock = Instantiate(spawnManager.blockPrefabs_[Random.Range(0, spawnManager.blockPrefabs_.Length)], transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveInput();
+        SpinInput();
+        DownMove();
+    }
+    
+    private void DownMove()
+    {
+        currentBlock.transform.position -= new Vector3(0, downSpeed * Time.deltaTime, 0);
+    }
+    
+    private void MoveInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentBlock.transform.position += Vector3.left * moveDistance;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            downSpeed *= downSpeedMutiple;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            downSpeed /= downSpeedMutiple;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentBlock.transform.position += Vector3.right * moveDistance;
+        }
+    }
+    
+    private void SpinInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            currentBlock.transform.rotation = Quaternion.Euler(0, 0, currentBlock.transform.rotation.eulerAngles.z + 90);
+        }
+    }
+}
