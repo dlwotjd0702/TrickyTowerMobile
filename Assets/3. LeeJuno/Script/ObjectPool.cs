61b6a21c84c93bc97025d5b8229697c0d944f793
juno11234 <cozam2@naver.com>
2025-05-15 17:57:48 +0900

    2025-05-15 PoolSetting

--- Assets/3. LeeJuno/Script/ObjectPool.cs
+++ Assets/3. LeeJuno/Script/ObjectPool.cs
@@ -2,16 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IObjectPoolItem
+{
+    string key { get; set; }
+    GameObject obj { get; }
+
+    void ReturnToPool();
+}
+
 public class ObjectPool : MonoBehaviour
 {
-    
-    void Start()
+    private Queue<IObjectPoolItem> Pool { get; set; }
+    private IObjectPoolItem Item { get; set; }
+    private Transform Parent { get; set; }
+    private byte ExpandSize { get; set; }
+
+    public void Initialize(IObjectPoolItem item, Transform parent, byte expandSize, string key)
     {
-        
+        Pool = new Queue<IObjectPoolItem>();
+        Item = item;
+        Item.obj.SetActive(false);
+        Parent = parent;
+        ExpandSize = expandSize;
+        Item.key = key;
+        Expand();
     }
-    
-    void Update()
+
+    public IObjectPoolItem GetItem()
     {
-        
+        if (Pool.Count == 0)
+        {
+            Expand();
+        }
+
+        return Pool.Dequeue();
+    }
+
+    private void Expand()
+    {
+        for (int i = 0; i < ExpandSize; i++)
+        {
+            var instance = GameObject.Instantiate(Item.obj, Parent).GetComponent<IObjectPoolItem>();
+            instance.key = Item.key;
+            ReturnToPool(instance);
+        }
+    }
+
+    public void ReturnToPool(IObjectPoolItem item)
+    {
+        item.obj.SetActive(false);
+        Pool.Enqueue(item);
     }
 }
